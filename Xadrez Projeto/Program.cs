102230f26916e5573787e457fb7b0fde29da4478
Lucas Ramos <lucas2adriano@gmail.com>
2021-08-18 20:58:29 -0300

    Criado a classe Posicao

--- Xadrez Projeto/Program.cs
+++ Xadrez Projeto/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using tabuleiro;
 
 namespace Xadrez_Projeto
 {
@@ -6,7 +7,10 @@ namespace Xadrez_Projeto
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Posicao P = new Posicao(3, 4);
+
+            Console.WriteLine("Posição da peça: " + P);
+
         }
     }
 }
