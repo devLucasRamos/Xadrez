aa62ac69a48c97b1706d1e44aa0c43dc665724f4
Lucas Ramos <lucas2adriano@gmail.com>
2021-08-18 20:46:14 -0300

    Projeto start

---
+++ Xadrez Projeto/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Xadrez_Projeto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
