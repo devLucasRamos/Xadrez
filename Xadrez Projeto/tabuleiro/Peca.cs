53056c1fa7188aaecd7b1fb4704cb3eaab93aeb0
Lucas Ramos <lucas2adriano@gmail.com>
2021-08-18 21:09:53 -0300

    Comecando a implementar o tabuleiro

---
+++ Xadrez Projeto/tabuleiro/Peca.cs
@@ -0,0 +1,18 @@
+﻿namespace tabuleiro
+{
+    class Peca
+    {
+        public Posicao posicao { get; set; }
+        public Cor cor { get; set; }
+        public int qteMovimentos { get; protected set; }
+        public Tabuleiro tab { get; set; }
+
+        public Peca(Posicao posicao, Cor cor,Tabuleiro tab)
+        {
+            this.posicao = posicao;
+            this.cor = cor;
+            qteMovimentos = 0;
+            this.tab = tab;
+        }
+    }
+}
---
+++ Xadrez Projeto/tabuleiro/Tabuleiro.cs
@@ -0,0 +1,16 @@
+﻿namespace tabuleiro
+{
+    class Tabuleiro
+    {
+        public int linhas { get; set; }
+        public int colunas { get; set; }
+        public Peca[,] pecas;
+
+        public Tabuleiro(int linhas, int colunas)
+        {
+            this.linhas = linhas;
+            this.colunas = colunas;
+            pecas = new Peca[linhas,colunas];
+        }
+    }
+}
