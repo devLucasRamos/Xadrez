a3f67228bbb82e70ca74cf8a993beef1e532f665
Lucas Ramos <lucas2adriano@gmail.com>
2021-08-19 11:05:58 -0300

    tratando erros com exception

--- Xadrez Projeto/Program.cs
+++ Xadrez Projeto/Program.cs
@@ -8,21 +8,31 @@ namespace Xadrez_Projeto
     {
         static void Main(string[] args)
         {
-            Tabuleiro tab = new Tabuleiro(8, 8);
 
-            tab.colocarPeca(new Rei(tab,Cor.Preta), new Posicao(0, 0));
-            tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
-            tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(2, 4));
-            tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(5, 0));
-            tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(4, 3));
-            tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(3, 4));
-            tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 5));
-            tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 4));
-            tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(2, 3));
+            try
+            {
+                Tabuleiro tab = new Tabuleiro(8, 8);
+
+                tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 0));
+                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
+                tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(2, 4));
+                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(5, 0));
+                tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(4, 3));
+                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(3, 4));
+                tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 5));
+                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 4));
+                tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(2, 3));
+       
 
 
-            Tela.imprimirTabuleiro(tab);
-            Console.WriteLine();
-        }
+                Tela.imprimirTabuleiro(tab);
+                Console.WriteLine();
+            }
+            catch(TabuleiroException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            }
+       
     }
 }
