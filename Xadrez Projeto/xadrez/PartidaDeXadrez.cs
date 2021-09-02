60a9a8d8c656e058f99ea319dc36e9af69e075d6
Lucas Ramos <lucas2adriano@gmail.com>
2021-09-02 00:13:38 -0300

    Jogada especial implementada promocao do peaogit add .!

--- Xadrez Projeto/xadrez/PartidaDeXadrez.cs
+++ Xadrez Projeto/xadrez/PartidaDeXadrez.cs
@@ -136,6 +136,24 @@ namespace pecas
                 desfazMovimento(origem, destino, pecaCapturada);
                 throw new TabuleiroException("Voce nao pode se colocar em xeque!!!");
             }
+
+            Peca p = tab.peca(destino);
+
+            // #jogadaespecial promocao
+            if (p is Peao)
+            {
+                if((p.cor == Cor.Branca && destino.linha == 0 || p.cor == Cor.Preta && destino.linha == 7))
+                {
+                    p = tab.retirarPeca(destino);
+                    pecas.Remove(p);
+                    Peca dama = new Dama(tab, p.cor);
+                    tab.colocarPeca(dama, destino);
+                    pecas.Add(dama);
+                }
+            }
+
+
+
             if (ReiemXeque(adversaria(jogadorAtual)))
             {
                 xeque = true;
@@ -153,7 +171,7 @@ namespace pecas
                 turno++;
                 mudaJogador();
             }
-            Peca p = tab.peca(destino);
+            
 
             // #jogadaespecial en passant
             if (p is Peao && (destino.linha == origem.linha - 2 || destino.linha == origem.linha + 2))
